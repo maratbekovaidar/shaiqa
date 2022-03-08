28fbe9295f21b4c31ed2c4feb57d32938a06571a
Aidar Maratbekov <maratbekovaidar@gmail.com>
2022-03-09 04:54:56 +0600

    fix(Uint8List): stream samples concat on one list and convert to base 64 feat(app): all work

--- lib/core/sound/sound_stream_controller.dart
+++ lib/core/sound/sound_stream_controller.dart
@@ -2,8 +2,10 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:mic_stream/mic_stream.dart';
+import 'package:shaiqa/constants/sounds/base64_sound.dart';
 import 'package:shaiqa/utils/services/music_detect_service.dart';
 
 class SoundStreamController {
@@ -22,19 +24,32 @@ class SoundStreamController {
 
     String result = "";
 
+    var soundBytes = BytesBuilder();
+
     listener = stream!.listen((samples) {
+      print(samples);
       result += base64Encode(samples);
       // print(base64Encode(samples));
+      soundBytes.add(samples);
     });
+
+
     Future.delayed(const Duration(seconds: 5), () {
       listener.cancel();
       print(result);
-      musicDetectService.detectSound(base64string: result);
+      musicDetectService.detectSound(base64string: base64Encode(soundBytes.toBytes()));
     });
   }
 
   Future stop() async {
     listener.cancel();
+  }
+
+  Future fileListen() async {
+    File imagefile = File("sounds/sound.mp3"); //convert Path to File
+    Uint8List imagebytes = await imagefile.readAsBytes(); //convert to bytes
+    String base64string = base64.encode(imagebytes); //convert bytes to base64 string
+    print(base64string);
   }
 
 }
